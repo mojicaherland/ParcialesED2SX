76eb417ac4bee52f91ec304de326752755055234
Herland Mojica Cardozo <mojicacardozoh@gmail.com>
2024-07-31 15:41:54 -0400

    añadiendo lcasi lo ultimo

--- src/tercerParcialGrafos/Main.java
+++ src/tercerParcialGrafos/Main.java
@@ -2,6 +2,18 @@ package tercerParcialGrafos;
 
 public class Main {
     public static void main(String[] args) {
+        /*
+         HAY MAS CODIGOS
+         DE GRAFOS EN LA CARPETA GRAFOS DE
+         ED2VargasPracticas
+        */
         Grafo grafo = new Grafo();
+
+        grafo.insertarNodo("A");
+        grafo.insertarNodo("B");
+        grafo.insertarArco("A", "B", 2);
+        grafo.insertarArco("D", "E", 1);
+        grafo.mostrarGrafo();
+        //System.out.println(grafo.cantidadArcos());
     }
 }
